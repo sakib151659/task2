0ffa04d33aa4b65653966028fd0619147b8abb35
sakib <sakib15-1659@diu.edu.bd>
2022-02-05 18:55:41 +0600

    sdk and gradle version updated

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -37,29 +37,7 @@ class _HomePageState extends State<HomePage> {
 
         ],
       ),
-        bottomNavigationBar: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(30), topLeft: Radius.circular(30)),
-              boxShadow: [
-                BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30.0),
-                topRight: Radius.circular(30.0),
-              ),
-              child: BottomNavigationBar(
-                items: <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.favorite), title: Text('Favourite')),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.favorite), title: Text('Favourite'))
-                ],
-              ),
-            )
-        )
+
     );
   }
 }
